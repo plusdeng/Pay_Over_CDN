eb9e48119f2ca515aeba933a7aa322c52dcccd9a
惶心 <30528433+hxcloud@users.noreply.github.com>
2018-04-24 10:56:51 +0100

    1.0 Release

---
+++ elements/config.js
@@ -0,0 +1,13 @@
+var setting = {
+    // 在以下双引号中粘贴QQ钱包收款链接
+    qqUrl: "https://i.qianbao.qq.com/wallet/sqrcode.htm?m=tenpay&a=1&u=1031092401&ac=CD3C9AE493BC106176F6B8F25E4230723AEA141B073F9DD9BC3747C823819645&n=Just%20your%20big%20fool.&f=wallet",
+
+    // 在以下双引号中粘贴微信收款链接
+    wechatUrl: "wxp://f2f0GmS4TQWrW_pacEXYhOOHSwjXlqUjv--L",
+
+    // 在以下双引号中粘贴支付宝收款链接
+    aliUrl: "HTTPS://QR.ALIPAY.COM/FKX05106TWOWYTJ6VELP79",
+
+    // 用于动态生成二维码的API，不需要修改。
+    qrcodeApi: "https://api.imjad.cn/qrcode/?&logo=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2Fhxco%2Fqrcode-solution%401.00%2Fqr.2heng.xin%2Ficon%2Fqr.png&size=300&level=L&bgcolor=%23ffffff&fgcolor=%23000000&text="
+}
