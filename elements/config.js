0c3977acd97c17b1a3192b5894274941498266b4
Focus Chen <30528433+hifocus@users.noreply.github.com>
2018-05-14 11:10:27 +0100

    1.1.2 Update: Text changes and new APIs

--- elements/config.js
+++ elements/config.js
@@ -1,6 +1,6 @@
 var setting = {
     // 在以下双引号中粘贴QQ钱包收款链接
-    qqUrl: "https://i.qianbao.qq.com/wallet/sqrcode.htm?m=tenpay&a=1&u=1031092401&ac=CD3C9AE493BC106176F6B8F25E4230723AEA141B073F9DD9BC3747C823819645&n=Just%20your%20big%20fool.&f=wallet",
+    qqUrl: "https://vac.qq.com/wallet/qrcode.htm?m=tenpay&a=1&u=1031092401&ac=1D504B80579C86BD5E96EBFC9C2B2E673AEA141B073F9DD9BC3747C823819645&n=%E5%BC%82%E4%B9%A1%E4%B9%8B%E4%BA%BA.&f=wallet",
 
     // 在以下双引号中粘贴微信收款链接
     wechatUrl: "wxp://f2f0GmS4TQWrW_pacEXYhOOHSwjXlqUjv--L",
@@ -9,5 +9,5 @@ var setting = {
     aliUrl: "HTTPS://QR.ALIPAY.COM/FKX05106TWOWYTJ6VELP79",
 
     // 用于动态生成二维码的API，不需要修改。
-    qrcodeApi: "https://api.imjad.cn/qrcode/?&logo=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2Fhxco%2Fqrcode-solution%401.00%2Fqr.2heng.xin%2Ficon%2Fqr.png&size=300&level=L&bgcolor=%23ffffff&fgcolor=%23000000&text="
+    qrcodeApi: "https://pan.baidu.com/share/qrcode?w=300&h=300&url="
 }
